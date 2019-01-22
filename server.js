f7060e55c6038a7af49125865dabc0a16810b191
Aria <aria@aria.nofriansyah>
2019-01-22 10:23:42 +0700

    commit 3

--- server.js
+++ server.js
@@ -2,14 +2,14 @@ const express = require('express');
 const app = express();
 const path = require('path');
 
-app.use(express.static(__dirname + './app'));
+app.use(express.static(__dirname + './'));
 
 app.listen(process.env.PORT || 8005);
 
 //pathLocationStrategy
 
 app.get('/*', function(req, res){
-     res.sendFile(path.join(__dirname + './app/app.component.html')); 
+     res.sendFile(path.join(__dirname + './app.component.html')); 
 })
 
 console.log('Console listening!');
