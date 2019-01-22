a9526be8e626d30a5cfcdc39134b9ea481b28cd8
Aria <aria@aria.nofriansyah>
2019-01-22 10:30:31 +0700

    edit server.js

--- server.js
+++ server.js
@@ -2,14 +2,14 @@ const express = require('express');
 const app = express();
 const path = require('path');
 
-app.use(express.static(__dirname + './'));
+app.use(express.static(__dirname + '/dist/myresume'));
 
 app.listen(process.env.PORT || 8005);
 
 //pathLocationStrategy
 
 app.get('/*', function(req, res){
-     res.sendFile(path.join(__dirname + './app.component.html')); 
+     res.sendFile(path.join(__dirname + '/dist/myresume/index.html')); 
 })
 
 console.log('Console listening!');
